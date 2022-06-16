9dfe9e251ed5a903b6b302c49da236010c34aa3c
Grzegorz Kulesza <kuleszagr@gmail.com>
2022-06-16 20:46:17 +0200

    added numbers validation for input fields, make input text fields responsive

--- kotlikeh/ViewController.swift
+++ kotlikeh/ViewController.swift
@@ -7,13 +7,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for i in 0..<5 {
+            DataTextFields[i].addTarget(self, action: #selector(ViewController.textFieldDidChange(_:)),  for: .editingChanged) //for change every times contains of text field changes
+            DataTextFields[i].delegate = self //for only digits can be tapped in field
+        }
+        
+        
     }
+    
+    
+    @IBOutlet var DataTextFields: [UITextField]!
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        
+        guard returnEmptyField() != 5 else {
+            return
+        }
+        DataTextFields[returnEmptyField()].text = String(returnEmptyField())
+    }
+    
 
-
+    func returnEmptyField() -> Int {
+        //        var timeTextField = DataTextFields[0]
+        //        var weightTextField = DataTextFields[1]
+        //        var temperatureStartTextField = DataTextFields[2]
+        //        var temperatureEndTextField = DataTextFields[3]
+        //        var powerTextField = DataTextFields[4]
+        var counter: Int = 0
+        var output: Int = 5 //5 is out of range
+        
+        for i in 0..<5 {
+            if DataTextFields[i].text == "" {
+                counter += 1
+                output = i
+            }
+        }
+        
+        if counter == 1 {
+            return output
+        } else {
+            return 5
+        }
+        
+    }
+    
+    //MARK: - UITextField Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        //For mobile numer validation
+        if textField == DataTextFields[0] || textField == DataTextFields[1] || textField == DataTextFields[2] || textField == DataTextFields[3] || textField == DataTextFields[4]
+        {
+            let allowedCharacters = CharacterSet(charactersIn:"0123456789")//Here change this characters based on your requirement
+            let characterSet = CharacterSet(charactersIn: string)
+            return allowedCharacters.isSuperset(of: characterSet)
+        }
+        return true
+    }
+    
+    
+    
 }
 
